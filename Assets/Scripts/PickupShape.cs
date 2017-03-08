2a5bbf69364baaa628fe062a026efacd24b7d360
morganhowell95 <morgan.howell95@gmail.com>
2017-03-08 14:31:22 -0500

    giving the FPC the ability to pick up chests which are puzzle pieces to vehicles

---
+++ Assets/Scripts/PickupShape.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupShape : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			PickupItem();
+		}
+	}
+
+
+	void PickupItem() {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit, 10)){
+			if (hit.collider.tag == "chest") {
+				Destroy (hit.collider.gameObject);
+			}
+		}
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		print ("test");
+		if(hit.collider.tag == "chest")
+		{
+			Destroy(hit.collider.gameObject);
+		}
+	}
+}
