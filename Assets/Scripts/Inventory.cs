195b53e003941fdc85d96c8a3bc2ce4e96539090
Sam L <Sam L>
2017-04-13 14:17:09 -0400

    started floating inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    private RectTransform inventoryRect;
+    private float inventoryWidth, inventoryHeight;
+    public int slots;
+    public int rows;
+    //alignment for slots
+    public float slotPaddingLeft, slotPaddingTop;
+    public float slotSize;
+
+    public GameObject slotPrefab;
+
+    //collection to contain all game objects
+    private List<GameObject> allSlots;
+
+	// Use this for initialization
+	void Start () {
+        CreateLayout();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void CreateLayout()
+    {
+        //place slots in inventory
+        allSlots = new List<GameObject>();
+
+        //calculate width/height/size of inventory
+        inventoryWidth = (slots / rows) * (slotSize + slotPaddingLeft) + slotPaddingLeft;
+        inventoryHeight = rows * (slotSize + slotPaddingTop) + slotPaddingTop;
+        inventoryRect = GetComponent<RectTransform>();
+
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryHeight);
+
+        
+    }
+}
